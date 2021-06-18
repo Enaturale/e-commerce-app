c8365bf0c1ab17c713af2b517d77eb79e0bd9f23
The Real Tech Lady <bukolaarowosebi@gmail.com>
2021-06-18 22:06:08 +0100

    Created cart page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,35 +1,69 @@
-import React from 'react'
-import { Container, Typography, Button, Grid } from '@material-ui/core'
+import React from "react";
+import { Container, Typography, Button, Grid } from "@material-ui/core";
+import useStyles from "./styles";
 
-const Cart = ({cart}) => {
-    const isEmpty = cart.line_items.length === 0; //variable to say that the cart is empty
+const Cart = ({ cart }) => {
+  const classes = useStyles();
+  //const isEmpty ; //variable to say that the cart is empty
 
-    //function to return jsx if the cart is found empty
-    const EmptyCart =() => {
-        <Typography variant ="subtitle1">You have no items in your cart. Why not add some?</Typography>
-    };
+  //function to return jsx if the cart is found empty
+  const EmptyCart = () => (
+    <Typography variant="subtitle1">
+     
+      You have no items in your cart. Why not add some?{" "}
+    </Typography>
+  );
 
-      //function to return jsx if the cart is found filled with items
-    const FilledCart =() => {
-        <>
-        <Grid container spacing={3}>
-            {cart.line_items.map((item) => (
-                <Grid item xs={12} sm={4} key={item.id}></Grid>
+  //function to return jsx if the cart is found filled with items
+  const FilledCart = () => (
+    <>
+      <Grid container spacing={3}>
+        {cart.line_items.map((item) => (
+          <Grid item xs={12} sm={4} key={item.id}>
+            <div>{item.name}</div>
+          </Grid>
+        ))}
+      </Grid>
 
-            ))}
+      <div className={classes.details}>
+        <Typography variant="h4">
+          Subtotal: {cart.subtotal.formatted_with_symbol}
+        </Typography>
+        <div>
+          <Button
+            className={classes.emptyButton}
+            variant="contained"
+            size="large"
+            type="button"
+            color="secondary"
+          >
+            Empty Cart
+          </Button>
+          <Button
+            className={classes.checkoutButton}
+            variant="contained"
+            size="large"
+            type="button"
+            color="primary"
+          >
+            Checkout
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 
-        </Grid>
-        </>
+  if(!cart.line_items) return "loading..."
 
-    };
+  return (
+    <Container>
+      <div className={classes.toolbar} />
+      <Typography className={classes.title} variant="h3">
+        Your Shopping Cart
+      </Typography>
+      {!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
+    </Container>
+  );
+};
 
-    return (
-        <Container>
-            <div className={classes.toolbar} />
-            <Typography className={classes.title} variant = "h3">Your Shopping Cart</Typography>
-            {isEmpty ? <EmptyCart/> : <FilledCart/> }
-        </Container>
-    )
-}
-
-export default Cart
+export default Cart;
