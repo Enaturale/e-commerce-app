924083380a2ddd1c143280b85aa9820905bb74c4
The Real Tech Lady <bukolaarowosebi@gmail.com>
2021-06-21 23:48:21 +0100

    Functional check out button leading to the check out page

--- src/components/CheckoutPart/Checkout/Checkout.jsx
+++ src/components/CheckoutPart/Checkout/Checkout.jsx
@@ -1,26 +1,42 @@
 import React, {useState} from 'react'
 import { Button, CircularProgress, Typography, Step, Stepper, Divider, Paper, StepLabel } from '@material-ui/core'
-import { classes } from 'istanbul-lib-coverage'
+
 import useStyles from './styles'
+import AddressForm from '../AddressForm';
+import PaymentForm from "../PaymentForm"
 
 const steps =['Shipping address' , 'Payment details']
 
 const Checkout = () => {
     const classes = useStyles();
-    const [activeStep, setActiveStep] = useState(0);
+    const [activeStep, setActiveStep] = useState(2);
+
+    const Form  =() => activeStep === 0
+    ? <AddressForm />
+    : <PaymentForm />
+
+    const Confrimation = () => (
+        <div>
+            Confrimation
+        </div>
+
+    )    
+
     return (
         <>
         <div className={classes.toolbar}/>
         <main className={classes.layout}>
             <Paper className={classes.paper}>
                 <Typography variant="h4" align="center">Checkout</Typography>
-                <Stepper activeStep={0} className={classes.stepper}>
+                <Stepper activeStep={activeStep} className={classes.stepper}>
                     {steps.map((step) => (
                         <Step key={step}>
                             <StepLabel>{step}</StepLabel>
                         </Step>
                     ))}
                 </Stepper>
+                {activeStep === steps.length ? <Confrimation /> : <Form /> }
+
             </Paper>
         </main>             
         </>
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 export {default as NavBar } from './NavBar/NavBar';
 export {default as Products} from './Products/Products';
 export {default as Cart} from './Cart/Cart';
+export {default as Checkout} from './CheckoutPart/Checkout/Checkout';
+
 
